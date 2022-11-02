16972eb01e8bc9a87f40c985b3defa564d695608
sovkino <garmadsa@yahoo.es>
2022-11-02 17:48:44 +0100

    Primera versión archivo java

---
+++ CrapSquared.java
@@ -0,0 +1,22 @@
+package CursoGIT;
+
+public class CrapSquared {
+
+	public int FakeSquare (int p_Factor)
+	{
+	int l_Retorno = 0;
+	
+	if (p_Factor==2) l_Retorno = 5;
+	else l_Retorno = p_Factor * p_Factor;
+	
+	return (l_Retorno);
+	}
+	
+	public static void main(String[] args)
+	{
+    CrapSquared l_Aplicacion = new CrapSquared();
+		System.out.println(l_Aplicacion.FakeSquare(2));
+		System.out.println(l_Aplicacion.FakeSquare(3));
+		
+	}
+}
